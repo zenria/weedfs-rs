e5878c9b66946f861577983bc966fc72efdab896
Philippe GASSMANN <philippe.gassmann@linkfluence.com>
2020-11-02 15:36:13 +0100

    impl write from file (very basic)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use crate::requests::AssignRequest;
-use crate::responses::{AssignResponse, LookupResponse};
+use crate::responses::{AssignResponse, LookupResponse, WriteResponse};
+use anyhow::anyhow;
 use anyhow::Context;
 use reqwest::Client;
+use std::fs::File;
+use std::io::Read;
 use std::time::Duration;
 use url::Url;
 
@@ -69,6 +72,68 @@ impl WeedFSClient {
             .context("assign response body read failed")?;
         Ok(response.to_result()?)
     }
+
+    pub async fn write(
+        &self,
+        assign_response: AssignResponse,
+        mut file: File,
+        filename: String,
+    ) -> anyhow::Result<u64> {
+        let meta = file.metadata()?;
+        if !meta.is_file() {
+            return Err(anyhow!("Not a file!"));
+        }
+        if meta.len() == 0 {
+            return Err(anyhow!("0-sized file is not allowed!"));
+        }
+
+        // build multipart shit
+        let mut bytes = Vec::new();
+        file.read_to_end(&mut bytes)?;
+
+        let part = reqwest::multipart::Part::bytes(bytes).file_name(sanitize_filename(filename));
+        let form = reqwest::multipart::Form::new().part("file", part);
+
+        let response = self
+            .http_client
+            .post(get_write_url(&assign_response)?)
+            .multipart(form)
+            .send()
+            .await
+            .context("unable to upload file")?;
+        let response = response
+            .json::<responses::Response<WriteResponse>>()
+            .await
+            .context("unable to read body")?;
+        Ok(response.to_result()?.size)
+    }
+}
+
+fn get_write_url(assign_response: &AssignResponse) -> anyhow::Result<Url> {
+    let mut url = String::new();
+    if !assign_response.public_url.starts_with("http") {
+        url.push_str("http://");
+    }
+    url.push_str(&assign_response.public_url);
+
+    url.push_str("/");
+    url.push_str(&assign_response.fid);
+
+    // FIXME here we should append some version on the end of the file id if the
+    //       version is > 0; unfortunately, I do not known to what it corresponds to...
+    //       Maybe the "count" field of the assign response... who knows...
+
+    Url::parse(&url).context("Unable to build write url")
+}
+
+fn sanitize_filename(filename: String) -> String {
+    if filename.len() == 0 {
+        "file".to_string()
+    } else if filename.len() > 255 {
+        filename[0..255].to_string()
+    } else {
+        filename
+    }
 }
 
 #[cfg(test)]
@@ -76,9 +141,30 @@ mod test {
     use crate::dir_status::Replication;
     use crate::requests::AssignRequestBuilder;
     use crate::WeedFSClient;
+    use std::fs::File;
 
     fn get_master_url() -> String {
         std::env::var("MASTER_URL").expect("MASTER_URL env variable is needed for running test")
+    }
+
+    #[tokio::test]
+    async fn test_assign_and_write() {
+        let cli = WeedFSClient::new(&get_master_url()).expect("Unable to build weed client");
+        let assign = cli
+            .assign(
+                AssignRequestBuilder::new()
+                    .collection("upload-original".to_string())
+                    .replication(Replication::OnceOnDifferentRack)
+                    .build(),
+            )
+            .await
+            .expect("Unable to assign");
+        let cargo = File::open("Cargo.toml").unwrap();
+        let byte_writted = cli
+            .write(assign, cargo, "Cargo.toml".to_string())
+            .await
+            .expect("Unable to write file");
+        dbg!(byte_writted);
     }
 
     #[tokio::test]
